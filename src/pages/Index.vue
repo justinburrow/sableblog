c309a648f698781569f4f6bbb96d6451c025e9f2
Justin Burrow <j.burrow@manduka.com>
2020-03-14 22:59:59 -0700

    Instagram section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,15 @@
 <template>
   <Layout>
-    <div class="home-post">
+    <section class="home-posts">
         <ul class="post-list">
           <li v-for="{ node } in $page.allWordPressPost.edges" :key="node.id">
             <HomePost :post="node" />
           </li>
         </ul>
-    </div>
+    </section>
+    <section class="instagram">
+      <h3>Join the Movement</h3>
+    </section>
   </Layout>
 </template>
 
@@ -59,6 +62,20 @@ export default {
       &:last-child {
         border: none;
       }
+    }
+  }
+  .instagram {
+    h3 {
+      background: #005851;
+      width: 645px;
+      margin: 80px auto 20px auto;
+      text-transform: uppercase;
+      font-size: 15px;
+      text-align: center;
+      padding: 7px 0 5px 0;
+      color: white;
+      letter-spacing: 1px;
+      font-weight: 300;
     }
   }
 </style>
