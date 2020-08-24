0964f094553c7f711cf0eee40d317654b6e9e9b2
Justin Burrow <j.burrow@manduka.com>
2020-08-23 21:03:51 -0700

    Changed Our Commitment link

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,7 +14,7 @@
         <div class="navigation only-desktop">
           <ul>
             <li class="blog" :class="blogHover ? 'active':''" @mouseenter="openBlogDropdown(), bloghover = true, searchHover = false" @click="openBlogDropdown(), bloghover = true, searchHover = false"><a>Blog</a></li>
-            <li class="commitment" @mouseenter="showDropdown = false, bloghover = false; searchHover = false;" @click="setActive('commitment')" :class="{active: isActive('commitment')}"><a href="/pages/our-commitment/">Our Commitment</a></li>
+            <li class="commitment" @mouseenter="showDropdown = false, bloghover = false; searchHover = false;" @click="setActive('commitment')" :class="{active: isActive('commitment')}"><a href="/our-commitment/">Our Commitment</a></li>
             <li class="about" @mouseenter="showDropdown = false, bloghover = false; searchHover = false;" @click="setActive('about')" :class="{active: isActive('about')}"><a href="/pages/about-us/">About</a></li>
           </ul>
           <div class="logo" @mouseenter="bloghover = false; searchHover = false;">
