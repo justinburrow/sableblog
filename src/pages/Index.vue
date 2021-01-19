244ba8b9918f2590c8394f657cfd806d6e6c7fbc
Justin Burrow <j.burrow@manduka.com>
2021-01-18 20:58:35 -0800

    limited number of posts on homepage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -98,8 +98,7 @@ export default {
   mounted() {
     this.filterPosts();
     this.uniquePosts = this.uniquePostFilter(this.filteredPosts, post => post.node.id);
-    this.uniquePosts.slice(0, 19);
-    console.log(this.isMobile);
+    this.uniquePosts = this.uniquePosts.slice(0, 25);
   }
 }
 </script>
