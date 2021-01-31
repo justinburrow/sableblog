68529bc9d296149e1c85da927a0678eb02904fe6
Justin Burrow <j.burrow@manduka.com>
2021-01-30 20:56:13 -0800

    Moved property evaluation

--- src/pages/Search.vue
+++ src/pages/Search.vue
@@ -78,6 +78,8 @@ export default {
         return e.node
       }
     }));
+  },
+  mounted() {
     this.getSearchResults();
   },
   watch: {
@@ -94,7 +96,6 @@ export default {
         query: this.searchingBy,
         limit: 30
       }, function(results) {
-        console.log(results);
         return results
       });
       this.$forceUpdate();
