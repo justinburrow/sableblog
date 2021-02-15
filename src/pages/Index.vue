5ffa66d5b8db0d36f579a6ae0e8a79c58d18f880
Justin Burrow <j.burrow@manduka.com>
2021-02-15 14:25:20 -0800

    Fix desktop blog sizing issue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,14 +12,14 @@
                   <a href="https://portenoire.co.uk" target="_blank"><img src="~@/assets/images/porte-noire.gif" alt="Porte Noire"></a>
                 </div>
 
-                <HomePost :post="post" />
+                <HomePost v-else :post="post" />
               </li>
             </span>
 
           <span v-show="isMobile" class="post-list">
             <li v-for="(post, index) in this.uniquePosts" :key="post.id" v-bind:class="{'wide-banner only-mobile': index == 8}">
               <a href="/coupledom/" class="coupledom-movement only-mobile" v-show="isMobile && index == 8">Coupledom</a>
-              <HomePost :post="post"/>
+              <HomePost :post="post"/>grid
             </li>
           </span>
         </ul>
