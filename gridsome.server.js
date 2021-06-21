26823695588152dff87c62199d73b0fbebab3090
Justin Burrow <j.burrow@manduka.com>
2021-06-21 06:09:17 -0700

    removed vuetify reference

--- gridsome.server.js
+++
@@ -1,7 +0,0 @@
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
-
-module.exports = (api) => {
-  api.chainWebpack((config, { isServer }) => {
-    config.plugin('vuetify-loader').use(VuetifyLoaderPlugin);
-  });
-}
