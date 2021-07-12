82956351894b2539ebef4d297fad885be103feb5
Justin Burrow <j.burrow@manduka.com>
2021-07-12 02:04:23 -0700

    fixed logo centering and trending features

--- src/components/TrendingFeatures.vue
+++ src/components/TrendingFeatures.vue
@@ -46,8 +46,54 @@ export default {
       required: true,
     },
   },
-  components: {
-
+  methods: {
+    setRowHeights() {
+      this.setRowBySection("h3");
+      this.setRowBySection(".excerpt");
+    },
+    setRowBySection(selector) {
+      var itemContentSections = document.querySelectorAll(selector);
+      // remove any previously set heights
+      itemContentSections.forEach(function(item){
+        item.style.height = "";
+      });
+      // set the maximum content section height for each row
+      var rows = [[]];
+      var rowMaxHeight = [0];
+      var currentRowIndex = 0;
+      var currentRowOffset;
+      var numberOfItems = itemContentSections.length;
+      itemContentSections.forEach(function(item, index){
+        var height = item.offsetHeight;
+        var offset = item.getBoundingClientRect().top;
+        if (currentRowOffset && currentRowOffset !== offset){
+          // this item is on the next row
+          // so increase the rowIndex and set the defaults
+          currentRowIndex += 1;
+          rows[currentRowIndex]=[];
+          rowMaxHeight[currentRowIndex]=0;
+        }
+        // add this item to the relevant row
+        rows[currentRowIndex].push(item);
+        // check for max section height on this row
+        rowMaxHeight[currentRowIndex] = (height > rowMaxHeight[currentRowIndex]) ? height : rowMaxHeight[currentRowIndex];
+        currentRowOffset = offset;
+      });
+      this.applyRowHeights(rows, rowMaxHeight);
+    },
+    applyRowHeights(rows, rowMaxHeight){
+      rows.forEach(function(row, index){
+        var maxHeight = rowMaxHeight[index];
+        row.forEach(function(item){
+          // set height minus 20px to account for padding
+          item.style.height = (maxHeight) + "px";
+        });
+      });
+    }
+  },
+  mounted() {
+    this.setRowHeights();
+    window.addEventListener("resize", this.setRowHeights());
   },
   filters: {
     removeHTML: function (val) {
